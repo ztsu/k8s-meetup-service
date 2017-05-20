1584469fc40dbd4c4dcb14cffe5caaa763607b8d
Alexey Golovnya <alexey.golovnya@gmail.com>
2017-05-20 15:57:23 +0700

    Move to Chi

--- main.go
+++ main.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"net/http"
+	"github.com/pressly/chi"
+	"github.com/pressly/chi/middleware"
+	"context"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello\n"))
+
+	if id, ok := r.Context().Value("id").(string); ok {
+		w.Write([]byte("Hello, " + id + "\n"))
+		return
+	} else {
+		w.Write([]byte("Hello guest\n"))
+	}
+}
+
+func mv(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, "id", "Qq")
+			next.ServeHTTP(w, r.WithContext(ctx))
+		},
+	)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(mv)
+	r.Get("/", handler)
+
+	http.ListenAndServe(":8080", r)
 }
